e2581791bd7e0da52183876cd880e49a301268c7
Your meena <meenapothappagari22@navgurukul.org>
2024-09-02 19:24:03 +0530

    heder

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from 'react'
 import Navbar from './components/Navbar'
+import Heder from './components/Heder'
 
 const App = () => {
   return (
     <div>
       <Navbar />
+      <Heder />
     </div>
   )
 }
